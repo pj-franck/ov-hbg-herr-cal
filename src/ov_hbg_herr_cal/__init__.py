764b876156f8ccb434a9a97590322d48f1070679
Per-Johan Franck <122224851+pj-franck@users.noreply.github.com>
2026-08-08 11:48:17 +0200

    Filter OV home matches

--- src/ov_hbg_herr_cal/__init__.py
+++ src/ov_hbg_herr_cal/__init__.py
@@ -1,5 +1,13 @@
 """Verktyg för OV Helsingborgs herrkalender."""
 
 from .source import DEFAULT_LEAGUE_SCHEDULE_URL, Match, ProfixioSource, SourceError
+from .filtering import CompetitionMatch, select_ov_home_matches
 
-__all__ = ["DEFAULT_LEAGUE_SCHEDULE_URL", "Match", "ProfixioSource", "SourceError"]
+__all__ = [
+    "CompetitionMatch",
+    "DEFAULT_LEAGUE_SCHEDULE_URL",
+    "Match",
+    "ProfixioSource",
+    "SourceError",
+    "select_ov_home_matches",
+]
