70b403bf57f0b568bbd8525f04e6df3ef45c9f0d
Per-Johan Franck <122224851+pj-franck@users.noreply.github.com>
2026-08-08 11:51:14 +0200

    Generate iCalendar feed

--- src/ov_hbg_herr_cal/__init__.py
+++ src/ov_hbg_herr_cal/__init__.py
@@ -2,6 +2,7 @@
 
 from .source import DEFAULT_LEAGUE_SCHEDULE_URL, Match, ProfixioSource, SourceError
 from .filtering import CompetitionMatch, select_ov_home_matches
+from .calendar import render_calendar
 
 __all__ = [
     "CompetitionMatch",
@@ -10,4 +11,5 @@ __all__ = [
     "ProfixioSource",
     "SourceError",
     "select_ov_home_matches",
+    "render_calendar",
 ]
