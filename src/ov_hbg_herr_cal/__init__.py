42af1157141f9873607c9b2c774b7b42b8a8abfc
Per-Johan Franck <122224851+pj-franck@users.noreply.github.com>
2026-08-08 11:55:15 +0200

    Publish calendar with GitHub Pages

--- src/ov_hbg_herr_cal/__init__.py
+++ src/ov_hbg_herr_cal/__init__.py
@@ -3,6 +3,7 @@
 from .source import DEFAULT_LEAGUE_SCHEDULE_URL, Match, ProfixioSource, SourceError
 from .filtering import CompetitionMatch, select_ov_home_matches
 from .calendar import render_calendar
+from .generate import calendar_from_league_matches
 
 __all__ = [
     "CompetitionMatch",
@@ -12,4 +13,5 @@ __all__ = [
     "SourceError",
     "select_ov_home_matches",
     "render_calendar",
+    "calendar_from_league_matches",
 ]
